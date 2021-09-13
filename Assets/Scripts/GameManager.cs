53372e3a2e30786a0c6307a589d8f4c511387b68
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-13 15:33:45 +0800

    3-Game Scenes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Bryce.Unity {
+    public class GameManager : MonoBehaviourPunCallbacks {
+
+        #region Photon Callbacks
+
+        public override void OnLeftRoom() {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom() {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+    }
+}
